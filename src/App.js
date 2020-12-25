51341051c8b6f1d0ab340685ca8558d24462d102
Arpan Neupane <arpanneupane19@gmail.com>
2020-12-25 13:33:26 -0600

    completed home section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AllPages from './components/AllPages'
 
+
 function App() {
   return (
     <div className="App">
--- src/components/AllPages.js
+++ src/components/AllPages.js
@@ -1,10 +1,50 @@
 import React from 'react'
 import Navbar from './Navbar';
+import Typewriter from 'typewriter-effect';
+import './AllPages.css'
 
 function AllPages() {
+    const strings = ['web apps.', 'GUIs.', 'React.', 'Python.', 'Flask.']
     return (
         <div>
             <Navbar/>
+
+            {/* Home */}
+            <section id='home'>
+                <div className='intro'>
+                    <p>Hello, I am</p>
+                    <h1>Arpan Neupane.</h1><br></br>
+
+                    <p>I am a programmer with interest in</p>
+                    <p className='strings'><Typewriter
+                                            options={{
+												strings: strings,
+												autoStart: true,
+												loop: true,
+										}}
+                    /></p>
+                </div>
+
+                <div className='button'>
+                    <a href='#about'>Learn More.</a>
+                </div>
+
+            </section>
+
+            {/* About */}
+            <section id='about'>
+
+            </section>
+        
+            {/* Projects */}
+            <section id='projects'>
+
+            </section>
+
+            {/* Contact */}
+            <section id='contact'>
+
+            </section>
         </div>
     )
 }
