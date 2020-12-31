1d4566312772fd40679d3349b873646bc5ae773a
Arpan Neupane <arpanneupane19@gmail.com>
2020-12-31 14:58:23 -0600

    added contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div className='contact'>
+            <div className='contact-header-info'>
+                <h1>Contact</h1>
+                <figcaption>You can contact me on these platforms.</figcaption>
+            </div>
+            <div className='contact-links'>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
