c928c3c38bff2327e5440a92479a3d60af46af80
Arpan Neupane <arpanneupane19@gmail.com>
2020-12-31 14:57:31 -0600

    added react router for navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import AllPages from './components/AllPages'
 import Navbar from './components/Navbar';
-
+import Contact from './components/Contact'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <AllPages/>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Navbar
+              home='/#home'
+              about='/#about'
+              projects='/#projects'
+            />
+            <AllPages/>
+          </Route>
+
+          <Route exact path='/contact'>
+            <Navbar 
+              home='/#home'
+              about='/#about'
+              projects='/#projects'
+            />
+            <Contact/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
