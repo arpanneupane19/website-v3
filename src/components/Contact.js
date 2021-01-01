f5de3d3b1781daeee6e7f279634eb8d733a6cf76
Arpan Neupane <arpanneupane19@gmail.com>
2020-12-31 23:51:58 -0600

    contact page finished!

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Contact.css';
+import { MdEmail } from "react-icons/md";
+import { GrInstagram } from 'react-icons/gr';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 function Contact() {
+
+    const [igData, setIgData] = useState([{}]);
+    
+    useEffect(()=> {
+        fetch('https://graph.instagram.com/<INSTAGRAM USER ID>?fields=username&access_token=<INSTAGRAM ACCESS TOKEN>').then(
+            response => response.json()
+        ).then(data => setIgData(data))
+    }, []);
+
+    const igURL = `https://instagram.com/${igData.username}`;
+
     return (
         <div className='contact'>
             <div className='contact-header-info'>
-                <h1>Contact</h1>
-                <figcaption>You can contact me on these platforms.</figcaption>
-            </div>
-            <div className='contact-links'>
+                <h1>Get in touch.</h1>
+            </div><br></br>
+            <div className='contact-links'> 
+                <div className='box'>
+                    <a href="mailto:arpanneupane19@gmail.com" target='__blank'><MdEmail/> Email</a>
+                </div>
+                <div className='box'>
+                    <a href={igURL} target="__blank"><GrInstagram/> Instagram</a>
+                </div>
+                <div className='box'>
+                    <a href="https://github.com/arpanneupane19" target='__blank'><AiOutlineGithub/> GitHub</a>
+                </div>
             </div>
         </div>
     )
