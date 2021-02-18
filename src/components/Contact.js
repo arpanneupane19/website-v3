a8a0c72fb3cd2e0fe60fd7ddb31b4bb11b05a6bf
Arpan Neupane <arpanneupane19@gmail.com>
2021-02-18 16:10:21 -0600

    formatting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,16 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './Contact.css';
 import { MdEmail } from "react-icons/md";
 import { GrInstagram } from 'react-icons/gr';
+import { GrYoutube } from 'react-icons/gr';
 import { AiOutlineGithub } from 'react-icons/ai';
 
 function Contact() {
 
     const [igData, setIgData] = useState([{}]);
-    
-    useEffect(()=> {
-        fetch('https://graph.instagram.com/<USER ID>?fields=username&access_token=<TOKEN>').then(
+
+    useEffect(() => {
+        fetch('https://graph.instagram.com/<USER_ID>?fields=username&access_token=<ACCESS_TOKEN>').then(
             response => response.json()
         ).then(data => setIgData(data))
     }, []);
@@ -21,15 +22,18 @@ function Contact() {
             <div className='contact-header-info'>
                 <h1>Get in touch.</h1>
             </div><br></br>
-            <div className='contact-links'> 
+            <div className='contact-links'>
                 <div className='box'>
-                    <a href="mailto:arpanneupane19@gmail.com" target='__blank'><MdEmail/> Email</a>
+                    <a href="mailto:arpanneupane19@gmail.com" target='__blank'><MdEmail /> Email</a>
                 </div>
                 <div className='box'>
-                    <a href={igURL} target="__blank"><GrInstagram/> Instagram</a>
+                    <a href={igURL} target="__blank"><GrInstagram /> Instagram</a>
                 </div>
                 <div className='box'>
-                    <a href="https://github.com/arpanneupane19" target='__blank'><AiOutlineGithub/> GitHub</a>
+                    <a href="https://youtube.com/ArpanNeupaneProductions" target="__blank"><GrYoutube /> YouTube</a>
+                </div>
+                <div className='box'>
+                    <a href="https://github.com/arpanneupane19" target='__blank'><AiOutlineGithub /> GitHub</a>
                 </div>
             </div>
         </div>
