5211845a86c1003e15c838e65b957d8e6d920a97
Arpan Neupane <arpanneupane19@gmail.com>
2021-02-18 16:10:02 -0600

    fixed formatting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../components/Navbar.css'
-import {FaGripLines} from 'react-icons/fa';
-import {AiOutlineClose} from 'react-icons/ai';
+import { FaGripLines } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 import Menu from './Menu'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Navbar({home, about, projects}) {
+function Navbar({ home, about, projects }) {
     const [showMenu, setShowMenu] = useState(false);
     const closeMenu = () => setShowMenu(false);
     return (
@@ -16,19 +16,19 @@ function Navbar({home, about, projects}) {
                 style={{
                     height: showMenu ? 'auto' : '80px'
                 }}
-            
+
             >
                 <div className='navigation-links'>
                     <nav>
                         <label className='logo'>Arpan Neupane</label>
                         <ul>
-                        <li><a href={home}>Home</a></li>
-                        <li><a href={about}>About</a></li>
-                        <li><a href={projects}>Projects</a></li>
-                        <li><Link to='/contact'>Contact</Link></li>
+                            <li><a href={home}>Home</a></li>
+                            <li><a href={about}>About</a></li>
+                            <li><a href={projects}>Projects</a></li>
+                            <li><Link to='/contact'>Contact</Link></li>
                         </ul>
-                        <li onClick={() => showMenu ? setShowMenu(false): setShowMenu(true)}className='bars'>{showMenu ? <AiOutlineClose/>: <FaGripLines/>}</li>
-                    </nav>  
+                        <li onClick={() => showMenu ? setShowMenu(false) : setShowMenu(true)} className='bars'>{showMenu ? <AiOutlineClose /> : <FaGripLines />}</li>
+                    </nav>
                 </div>
 
                 <div className='menu'>
@@ -40,10 +40,10 @@ function Navbar({home, about, projects}) {
             </div>
 
         </div>
-        
-                
 
-        
+
+
+
 
 
 
