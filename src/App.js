0cbed22d225d16fd395bc67d98e3556dc749a2ad
Arpan Neupane <arpanneupane19@gmail.com>
2021-04-30 21:11:58 -0500

    added react-https-redirect

--- src/App.js
+++ src/App.js
@@ -3,32 +3,35 @@ import AllPages from './components/AllPages'
 import Navbar from './components/Navbar';
 import Contact from './components/Contact'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Navbar
-              home='/#home'
-              about='/#about'
-              projects='/#projects'
-            />
-            <AllPages />
-          </Route>
+    <HttpsRedirect>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Navbar
+                home='/#home'
+                about='/#about'
+                projects='/#projects'
+              />
+              <AllPages />
+            </Route>
 
-          <Route exact path='/contact'>
-            <Navbar
-              home='/#home'
-              about='/#about'
-              projects='/#projects'
-            />
-            <Contact />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+            <Route exact path='/contact'>
+              <Navbar
+                home='/#home'
+                about='/#about'
+                projects='/#projects'
+              />
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </HttpsRedirect>
   );
 }
 
